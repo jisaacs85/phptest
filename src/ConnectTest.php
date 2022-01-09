9c276b5245f703b99a0f32b4f6bbf0518a794fc7
Joseph Isaacs <isaacs_joseph@hotmail.com>
2022-01-08 21:36:33 -0500

    Modify url to microsoft

--- src/ConnectTest.php
+++ src/ConnectTest.php
@@ -13,7 +13,7 @@ class RemoteConnectTest extends TestCase
   {
     // test to ensure that the object from an fsockopen is valid
     $connObj = new RemoteConnect();
-    $serverName = 'www.google.com';
+    $serverName = 'www.microsoft.com';
     $this->assertTrue($connObj->connectToServer($serverName) !== false);
   }
 }
